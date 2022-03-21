a71912bd92c71871d72561e825e55530fef85b1a
textu <texturich@gmail.com>
2022-03-21 05:06:32 +0300

    Упрощён schedule_change_checker: процесс ночного обновления в "тихую" заменён на фильтр. Добавлен флаг отвечающий за то, было ли обновлено расписание в прошлый раз. Если нет, то оно обновляется "втихую".

--- Handlers/schedule_change_checker.py
+++ Handlers/schedule_change_checker.py
@@ -24,30 +24,49 @@ class ScheduleChecker:
         self._database = database
         self._chsu_api = chsu_api
         self._date_handler = DateHandler()
-        self.updating = event_loop.create_task(self._daily_updating_process())
         self.checking = event_loop.create_task(self._check_updates_process())
+        self._is_updated = True
 
     def get_status(self):
-        return 'working' if not self.updating.done() and not self.checking.done() else 'not working'
+        return 'working' if not self.checking.done() else 'not working'
 
-    async def _daily_updating_process(self):
+    async def _check_updates_process(self):
         while True:
             try:
-                await self._update_hashes()
-            except ConnectionError as err:
-                print(err)
-            await asyncio.sleep(59)
+                self._date_handler = DateHandler()
+                await self._check_updates()
+            except Exception as err:
+                print(f"{err.__class__.__name__}: {err}")
+                self._is_updated = False
+            await asyncio.sleep(1)
 
-    async def _update_hashes(self):
-        if await self._is_midnight():
+    async def _check_updates(self):
+        if await self._is_update_time():
             group_list = await self._database.get_groups_list()
             for group in group_list:
-                await self._update_group_and_get_changes(group)
+                await self._check_and_send_updates_for_group(group)
+        self._is_updated = True
 
-    async def _is_midnight(self):
+    async def _is_update_time(self):
         return \
-            self._date_handler.get_current_date_object().hour == 0 and \
-            self._date_handler.get_current_date_object().minute == 0
+            self._date_handler.get_current_date_object().second == 0 and \
+            self._date_handler.get_current_date_object().hour != 0 and \
+            self._date_handler.get_current_date_object().minute % 20 == 0
+
+    async def _check_and_send_updates_for_group(self, group):
+        if not self._is_updated:
+            users = await self._database.get_check_changes_members(group)
+            response = await self._get_new_schedules(group)
+            await self._send_responses(users, response)
+
+    async def _get_new_schedules(self, group):
+        response = []
+        for time in await self._update_group_and_get_changes(group) or []:
+            for schedule in await self._chsu_api.get_schedule_list_string(group, time):
+                response.append(
+                    (f"Обновленное расписание:\n\n{schedule['text']}", schedule['callback_data'])
+                )
+        return response
 
     async def _update_group_and_get_changes(self, group_name):
         self._date_handler.parse_interval(days=14)
@@ -55,7 +74,12 @@ class ScheduleChecker:
             new_hashes = await self._chsu_api.get_schedule_list_hash(group_name, *self._date_handler.get_string())
             group_obj = await self._database.get_group_hashes(group_name)
             await self._database.set_group_hashes(new_hashes, group_name)
-            return self._get_difference_dates(new_hashes, group_obj)
+            return list(filter(
+                lambda x:
+                x != self._date_handler.parse_interval(days=14)[1].strftime("%d.%m.%Y") and
+                self._date_handler.get_current_date_object().hour == 0,
+                self._get_difference_dates(new_hashes, group_obj)
+            ))
         except EmptyResponse as err:
             print(f"{err.__class__.__name__}: {err}")
             return []
@@ -82,41 +106,6 @@ class ScheduleChecker:
     @staticmethod
     def _get_date_strings(object_list: list) -> list:
         return list(map(lambda date_and_hash: date_and_hash['time'].strftime("%d.%m.%Y"), object_list))
-
-    async def _check_updates_process(self):
-        while True:
-            try:
-                await self._check_updates()
-            except ConnectionError as err:
-                print(err)
-            self._date_handler = DateHandler()
-            await asyncio.sleep(1)
-
-    async def _check_updates(self):
-        if await self.is_beginning_of_the_hour():
-            group_list = await self._database.get_groups_list()
-            for group in group_list:
-                await self._check_and_send_updates_for_group(group)
-
-    async def is_beginning_of_the_hour(self):
-        return \
-            self._date_handler.get_current_date_object().second == 0 and \
-            self._date_handler.get_current_date_object().hour != 0 and \
-            self._date_handler.get_current_date_object().minute % 20 == 0
-
-    async def _check_and_send_updates_for_group(self, group):
-        users = await self._database.get_check_changes_members(group)
-        response = await self._get_new_schedules(group)
-        await self._send_responses(users, response)
-
-    async def _get_new_schedules(self, group):
-        response = []
-        for time in await self._update_group_and_get_changes(group) or []:
-            for schedule in await self._chsu_api.get_schedule_list_string(group, time):
-                response.append(
-                    (f"Обновленное расписание:\n\n{schedule['text']}", schedule['callback_data'])
-                )
-        return response
 
     async def _send_responses(self, users, response):
         for message in response:
